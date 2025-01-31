10b30d4c862e8ba8c780e5517f519e44912e9af3
devopsvenkat1488 <venkatesh.sarivisetty015@gmail.com>
2025-01-31 06:36:27 -0500

    Build updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from GitHub Actions CI/CD!");
+    res.send("Hello from NEW GitHub Actions CI/CD!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
